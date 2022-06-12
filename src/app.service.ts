4694525e11cd9ea97a07e26f9acadfb0814a07ce
PrinceFodeke <fodekep@gmail.com>
2022-06-12 15:18:50 -0400

    publish message capability

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PubSub } from '@google-cloud/pubsub';
+import { ConfigService } from '@nestjs/config'
+import { publishDto } from 'dtos/publishMessage';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  constructor(private configService: ConfigService) {};
+  private pubSubClient = new PubSub();
+  private topicNameOrId = this.configService.get('TOPICNAMEORID');
+
+  async publishMessage(publishDto: publishDto) {
+    const message = publishDto.message;
+    const dataBuffer = Buffer.from(message);
+    try {
+      const messageId = await this.pubSubClient
+        .topic(this.topicNameOrId)
+        .publishMessage({data: dataBuffer});
+      console.log(`Message ${messageId} published.`);
+    } catch (error) {
+      console.error(`Received error while publishing: ${error.message}`);
+      process.exitCode = 1;
+    }
   }
+
+  async getHello() {
+    return 'Hello'
+  }
+  
 }
